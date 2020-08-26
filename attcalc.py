656cc1f7af53ddd8879a45578fbf83b2a21f8b8f
Aleksander Alekseev <mail@eax.me>
2020-08-26 19:49:50 +0300

    attcalc.py: suggests closest std values

--- attcalc.py
+++ attcalc.py
@@ -3,6 +3,28 @@
 import argparse
 from math import pow, log10
 
+# standard 1% resistors values (Ohm)
+std_values = [
+	10,11,12,13,15,16,18,20,22,24,27,30,33,36,39,43,47,51,56,62,68,75,82,91,
+	100,110,120,130,150,160,180,200,220,240,270,300,330,360,390,430,470,
+	510,560,620,680,750,820,910,
+	1000,1100,1200,1300,1500,1600,1800,2000,2200,2400,2700,3000,3300,3600,3900,
+	4300,4700,4750,5100,5600,6200,6800,7500,8200,9100,
+	10000,11000,12000,13000,15000,16000,18000,20000,22000,24000,27000,
+	30000,33000,36000,39000,43000,47000,51000,56000,62000,68000,75000,
+	82000,91000,
+	100000,110000,120000,130000,150000,160000,180000,200000,220000,240000,270000,
+	300000,330000,360000,390000,430000,470000,510000,560000,620000,680000,750000,
+	820000,910000,
+	1_000_000,1_100_000,1_200_000,1_500_000,1_600_000,1_800_000,2_000_000,
+	2_200_000,2_400_000,2_700_000,3_000_000,3_300_000,5_100_000,
+]
+
+def closest(val):
+	diffs = [ (x, abs(x-val)) for x in std_values ]
+	sorted_vals = sorted([ (x, abs(x-val)) for x in std_values ], key = lambda a: a[1])
+	return sorted_vals[0][0]
+
 parser = argparse.ArgumentParser(
     description='Attenuator calculator'
     )
@@ -31,8 +53,8 @@ PI-network:             T-network:
 
 """)
 
-print("R1 = R2 = {:.2f} Ohm".format(R1))
-print("R3 = {:.2f} Ohm".format(R3))
+print("R1 = R2 = {:.2f} Ohm, closest: {} Ohm".format(R1, closest(R1)))
+print("R3 = {:.2f} Ohm, closest: {} Ohm".format(R3, closest(R3)))
 print(" ")
-print("R4 = R5 = {:.2f} Ohm".format(R4))
-print("R6 = {:.2f} Ohm".format(R6))
+print("R4 = R5 = {:.2f} Ohm, closest: {} Ohm".format(R4, closest(R4)))
+print("R6 = {:.2f} Ohm, closest: {} Ohm".format(R6, closest(R6)))
