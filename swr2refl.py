710240199fc077cd6b990365e22d5c3f2266694f
Aleksander Alekseev <mail@eax.me>
2021-04-11 11:35:56 +0300

    Minor changes

--- swr2refl.py
+++ swr2refl.py
@@ -7,5 +7,5 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 swr = float(sys.argv[1])
-loss = (swr-1)*(swr-1)/((swr+1)*(swr+1))
-print("Reflected power: {:.2f}%".format(100*loss))
+refl = (swr-1)*(swr-1)/((swr+1)*(swr+1))
+print("Reflected power: {:.2f}%".format(100*refl))
